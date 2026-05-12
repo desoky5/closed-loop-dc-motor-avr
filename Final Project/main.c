d06a9a3c3e841c59527f069aa18e72be358ddd98
Omar Desoky <omardsoky05@gmail.com>
2026-05-12 21:28:23 +0300

    DC motor control using switches for speed up and speed down added

--- Final Project/main.c
+++ Final Project/main.c
@@ -6,13 +6,81 @@
  */ 
 
 #include <avr/io.h>
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+#include <stdlib.h>
+#include <stdio.h>
 
+/*------------------ Macros---------------*/
+#define setbit(port,bit) (port) |= (1<<(bit))
+#define clearbit(port,bit) (port) &= ~(1<<(bit))
+#define bitisset(reg,bit) ((reg)&(1<<(bit)))
+#define bitisclear(reg,bit) !((reg)&(1<<(bit)))
+
+volatile int PWMduty =0 ; // -255 TO +255 , sign = direction
+
+void PWM_init()
+{
+	TCCR0 = (1<<WGM00) | (1<<WGM01) | (1<<COM01) |(1<<CS01); /* Change the prescaler to make the intution of changing the frequency*/
+	setbit(DDRB,PB3) ;
+}
 
 int main(void)
-{
-    /* Replace with your application code */
+{   
+	PWM_init();
+	
+	// H Bridge control pins Config.
+	DDRB = 0XFF ; // The port that has pins for motor direction control
+	
+	clearbit(DDRD,PD2); //set PD2 as input
+	setbit(PORTD,PD3); //Enables PD2 PULL UP
+	clearbit(DDRD,PD3); //set PD3 as input
+	setbit(PORTD,PD2); //Enables PD2 PULL UP
+	
+		
+	/*--------Enable Interrupts-----*/
+	MCUCR = 0x0A ; // Make INT0 & INT1 Falling Edge triggered
+	setbit(GICR,INT0); // Enables INT0 External Interrupt (PD2 : Speed Up switch)
+	setbit(GICR,INT1); // Enables INT1 External Interrupt (PD3 : Speed Down Switch)
+		
+	sei(); // Enables the global interrupt
+	
     while (1) 
     {
+		/* H Bridge Logic */
+		if (PWMduty>0)
+		{
+			setbit(PORTB,PB0);
+			clearbit(PORTB,PB1); //Motor Clockwise
+			OCR0 = PWMduty;
+		}
+		if (PWMduty<0)
+		{
+			setbit(PORTB,PB1);
+			clearbit(PORTB,PB0); //Motor AntiClockwise
+			OCR0 = abs(PWMduty);
+		}
+		if (PWMduty==0)
+		{
+			clearbit(PORTB,PB1);
+			clearbit(PORTB,PB0); //Motor stops
+			OCR0 = 0;
+		}
     }
 }
 
+ISR(INT0_vect)
+{
+	PWMduty = PWMduty + 25 ;
+	if (PWMduty>255) PWMduty = 255 ;
+	while(bitisclear(PIND,PD2));
+}
+
+
+ISR(INT1_vect)
+{
+	PWMduty = PWMduty - 25 ;
+	if (PWMduty < -255) PWMduty = -255 ;
+	while(bitisclear(PIND,PD3)) ;
+}
