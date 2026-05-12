69e48a7843858e923ccca98d23e0cd70c4baff8c
Omar Desoky <omardsoky05@gmail.com>
2026-05-12 21:10:20 +0300

    Initial Commit of avr atmega32 embedded systems project

---
+++ Final Project/main.c
@@ -0,0 +1,18 @@
+/*
+ * Final Project.c
+ *
+ * Created: 5/12/2026 9:01:59 PM
+ * Author : Omar Desoky
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
